2449a0cddc0011f7656c3fb2fd56ab4122e35a77
Rob Derickson <rderickson@erickson.com>
2022-06-14 18:47:57 -0400

    Fixed a typo. Added code to create the CurrentUserAllHosts profile if it does not exist.

--- setup-psprofile.ps1
+++ setup-psprofile.ps1
@@ -1,3 +1,6 @@
-Copy-Item $PSScriptRoot\psrpofile\robprofile.ps1 $env:OneDrive
+Copy-Item $PSScriptRoot\psprofile\robprofile.ps1 $env:OneDrive
 Copy-Item $PSScriptRoot\psprofile\oh-my-posh.yaml $env:OneDrive
-Copy-Item $PSScriptRoot\psprofile\profile.ps1 $PROFILE.CurrentUserAllHosts
+If (-not Test-Path $PROFILE.CurrentUserAllHosts) {
+    New-Item -Type File $PROFILE.CurrentUserAllHosts -Force | Out-Null
+}
+Copy-Item $PSScriptRoot\psprofile\profile.ps1 $PROFILE.CurrentUserAllHosts -Force
