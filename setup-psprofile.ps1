69f283348d086baf1cb56afda44e6e1f3715312d
Rob Derickson <rderickson@erickson.com>
2022-06-14 19:26:57 -0400

    Added parentheses around Test-Path condition.

--- setup-psprofile.ps1
+++ setup-psprofile.ps1
@@ -1,6 +1,6 @@
 Copy-Item $PSScriptRoot\psprofile\robprofile.ps1 $env:OneDrive
 Copy-Item $PSScriptRoot\psprofile\oh-my-posh.yaml $env:OneDrive
-If (-not Test-Path $PROFILE.CurrentUserAllHosts) {
+if (-not (Test-Path $PROFILE.CurrentUserAllHosts)) {
     New-Item -Type File $PROFILE.CurrentUserAllHosts -Force | Out-Null
 }
 Copy-Item $PSScriptRoot\psprofile\profile.ps1 $PROFILE.CurrentUserAllHosts -Force
